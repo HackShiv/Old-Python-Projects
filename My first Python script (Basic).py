ad6da566d99b7da3a14c0bb782759d853dd1c68f
HackerShiv <107373873+HackShiv@users.noreply.github.com>
2024-03-03 22:09:15 +0000

    Installing pi-hole on windows with Python
    Worked for a bit

---
+++ My first Python script (Basic).py
@@ -0,0 +1,18 @@
+#conversation
+
+
+print ('Hello, what is your name?')
+TheName = input ('Name:')
+str(print ('Nice to meet you, ' + TheName))
+print ('Your name is ' + str(len(TheName)))
+print ('characters long..')
+
+print ('What is your age?')
+age = int(float(input('Enter your age:')))
+
+
+    
+end = print('Goodbye')
+
+
+# From Shiv
